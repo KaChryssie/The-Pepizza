94a08ca48c891fc1aca98b7c8418aa94f9deedb5
KaChryssie <karerassie@gmail.com>
2020-10-11 23:05:48 +0200

    add the crust variable

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ var pizzaBasePrice = {
     large: 5000,
 };
 
+var crust = {
+    small: {
+        "Pizza Bagels" : 300,
+        "Thin Crust" : 400,
+        "Neapolitan Crust" : 505,
+        "Flatbread" : 350,
+    },
+    medium: {
+        "Pizza Bagels" : 500,
+        "Thin Crust" : 600,
+        "Neapolitan Crust" : 605,
+        "Flatbread" : 850,
+    },
+    large: {
+        "Pizza Bagels" : 700,
+        "Thin Crust" : 800,
+        "Neapolitan Crust" : 705,
+        "Flatbread" : 950,
+    }
+}
