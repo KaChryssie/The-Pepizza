d90ef8debf6e7079476d63b565b203d832634107
KaChryssie <karerassie@gmail.com>
2020-10-11 23:14:19 +0200

    add the delivery prototype

--- js/script.js
+++ js/script.js
@@ -116,3 +116,8 @@ function Order(pizzaStyle, quantity){
     this.needsDelivery = false;
     this.deliveryLocation = "";
 }
+
+Order.prototype.setDelivery = function(needsDelivery, deliveryLocation){
+    this.needsDelivery = needsDelivery;
+    this.deliveryLocation = deliveryLocation;
+}
